ac203eeed13550365d8719206564774712fb6862
admin-ll55 <36874774+admin-ll55@users.noreply.github.com>
2020-03-05 10:44:39 +0800

    Update minify_rs.py

--- ckplayerx/minify_rs.py
+++ ckplayerx/minify_rs.py
@@ -1,6 +1,6 @@
 import re
 import sys
 t = open(sys.argv[1], "rb").read().decode()
-t = re.sub(r"  +", " ", t)
 t = re.sub(r"\n", "", t)
+t = re.sub(r"  +", " ", t)
 open(sys.argv[1].replace(".draft", ""), "wb").write(t.encode())
