aa93ec4551fcb73011666fdc0a627978579ba3c4
admin-ll55 <36874774+admin-ll55@users.noreply.github.com>
2020-03-05 10:51:38 +0800

    Add files via upload

--- ckplayerx/minify_rs.py
+++ ckplayerx/minify_rs.py
@@ -1,6 +1,8 @@
 import re
 import sys
+if ".draft" not in sys.argv[1]:
+  sys.exit(1)
 t = open(sys.argv[1], "rb").read().decode()
-t = re.sub(r"\n", "", t)
+t = re.sub(r"(\n|\r)", "", t)
 t = re.sub(r"  +", " ", t)
 open(sys.argv[1].replace(".draft", ""), "wb").write(t.encode())
